ea335b7508ba6161fbd623d76e82d50c101aaca6
Sudini Hemanth Reddy <sudinihemanth@gmail.com>
2020-11-23 12:13:02 +0530

    Pop Up

--- js/script.js
+++ js/script.js
@@ -259,58 +259,12 @@ $(function(){
 
 });
 
-// Google Maps
-google.maps.event.addDomListener(window, 'load', loadGoogleMap);
-function loadGoogleMap() {    
-    $('#map').addClass('loading');    
-    var latlng = new google.maps.LatLng(40.6700, -73.9400); // New York
-    var settings = {
-        zoom: 14,
-        center: new google.maps.LatLng(40.6700, -73.9400),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl: false,
-        scrollwheel: false,
-        draggable: true,
-        //styles: [{featureType:'all',stylers:[{saturation:-100},{gamma:0.50}]}],
-        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-        navigationControl: false,
-        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},            
-    };
-    var map = new google.maps.Map(document.getElementById("map"), settings);
-
-    google.maps.event.addDomListener(window, "resize", function() {
-        var center = map.getCenter();
-        google.maps.event.trigger(map, "resize");
-        map.setCenter(center);
-        $('#map').removeClass('loading');
+$(document).ready(function(){
+    // Show the Modal on load
+    $("#myModal").modal("show");
+      
+    // Hide the Modal
+    $("#myBtn").click(function(){
+      $("#myModal").modal("hide");
     });
-
-    var contentString =
-        '<div id="info-window">'+
-        '<p>18 McLuice Road, Vellyon Hills,<br /> New York, NY 10010<br /><a href="https://plus.google.com/102896039836143247306/about?gl=za&hl=en" target="_blank">Get directions</a></p>'+
-        '</div>';
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    var companyImage = new google.maps.MarkerImage('https://themeselection.com/demo/ashoka-html-template/images/map-marker.png',
-        new google.maps.Size(36,62),// Width and height of the marker
-        new google.maps.Point(0,0),
-        new google.maps.Point(18,52)// Position of the marker 
-    );
-
-    var companyPos = new google.maps.LatLng(40.6700, -73.9400);
-
-    var companyMarker = new google.maps.Marker({
-        position: companyPos,
-        map: map,
-        icon: companyImage,
-        title:"Shapeshift Interactive",
-        zIndex: 3});
-
-    google.maps.event.addListener(companyMarker, 'click', function() {
-        infowindow.open(map,companyMarker);
-        pageView('/#address');
-    });
-
-};
+  });
